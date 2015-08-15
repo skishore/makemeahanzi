f0f2477b889c379b552b06da6a37c898132deadb
Shaunak Kishore <kshaunak@gmail.com>
2015-08-15 13:41:54 -0400

    Add script to extract characters

---
+++ getchar.py
@@ -0,0 +1,45 @@
+#!/usr/bin/python
+'''
+Extracts one or more characters from each of the svg fonts in the SVG directory
+and packages them into a 'chars.html' output file.
+'''
+import os
+import sys
+
+SCALE = 0.16
+SVG_DIR = 'derived'
+
+if __name__ == '__main__':
+  assert len(sys.argv) > 1, 'Usage: ./getchar.py <unicode_codepoint>+'
+  svgs = [file_name for file_name in os.listdir(SVG_DIR)
+          if file_name.endswith('.svg') and not file_name.startswith('.')]
+  glyphs = []
+  for file_name in svgs:
+    glyphs.append([])
+    with open(os.path.join(SVG_DIR, file_name)) as file:
+      data = file.read()
+    for codepoint in sys.argv[1:]:
+      index = data.find('unicode="&#x{0};"'.format(codepoint))
+      if index < 0:
+        print >> sys.stderr, '{0}: missing {1}'.format(file_name, codepoint)
+        continue
+      (left, right) = ('<glyph', '/>')
+      (start, end) = (data.rfind(left, 0, index), data.find(right, index))
+      if start < 0 or end < 0:
+        print >> sys.stderr, '{0}: malformed {1}'.format(file_name, codepoint)
+        continue
+      glyphs[-1].append(data[start:end + len(right)])
+
+  with open('chars.html', 'w') as f:
+    f.write('<!DOCTYPE html>\n  <html>\n    <body>\n')
+    for row in glyphs:
+      f.write('      <div>\n')
+      for glyph in row:
+        size = int(1024*SCALE)
+        f.write('        <svg width="{0}" height="{0}">\n'.format(size))
+        f.write('          {0}\n'.format(glyph.replace(
+            '<glyph', '<path transform="scale({0:.2g}, -{0:0.2g}) '
+            'translate(0, -800)"'.format(SCALE))))
+        f.write('        </svg>\n')
+      f.write('      </div>\n')
+    f.write('    </body>\n  </html>')
