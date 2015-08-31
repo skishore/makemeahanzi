79b05c089d04e294b65e60a99064f84e72603b2a
Shaunak Kishore <kshaunak@gmail.com>
2015-08-31 02:08:13 -0400

    Add helpers to skip verified glyphs

--- server/glyph.js
+++ server/glyph.js
@@ -7,10 +7,24 @@ Meteor.methods({
     var next = Glyphs.findOne({name: {$gt: name}}, {sort: {name: 1}});
     return next ? next : Glyphs.findOne({}, {sort: {name: 1}});
   },
+  get_next_glyph_skip_verified: function(glyph) {
+    var name = glyph ? glyph.name : undefined;
+    var next = Glyphs.findOne(
+      {name: {$gt: name}, 'manual.verified': {$ne: true}}, {sort: {name: 1}});
+    return next ? next : Glyphs.findOne(
+      {'manual.verified': {$ne: true}}, {sort: {name: 1}});
+  },
   get_previous_glyph: function(glyph) {
     var name = glyph ? glyph.name : undefined;
     var prev = Glyphs.findOne({name: {$lt: name}}, {sort: {name: -1}});
     return prev ? prev : Glyphs.findOne({}, {sort: {name: -1}});
+  },
+  get_previous_glyph_skip_verified: function(glyph) {
+    var name = glyph ? glyph.name : undefined;
+    var prev = Glyphs.findOne(
+      {name: {$lt: name}, 'manual.verified': {$ne: true}}, {sort: {name: -1}});
+    return prev ? prev : Glyphs.findOne(
+      {'manual.verified': {$ne: true}}, {sort: {name: -1}});
   },
 });
 
