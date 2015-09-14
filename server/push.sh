3b0f9bda21978868adb92f6da416957d5b2a0a71
Shaunak Kishore <kshaunak@gmail.com>
2015-09-13 21:48:41 -0400

    Add push script

---
+++ server/push.sh
@@ -0,0 +1,3 @@
+#!/bin/bash
+COMMAND=`meteor mongo -U han.meteor.com | tail -1 | sed 's_mongodb://\([a-z0-9\-]*\):\([a-f0-9\-]*\)@\(.*\)/\(.*\)_mongorestore -u \1 -p \2 -h \3 -d \4_'`
+$COMMAND --drop server/backup/meteor
