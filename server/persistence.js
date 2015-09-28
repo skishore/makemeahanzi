e96f11720c0cd8a95dc31ae26a54f495aaf7ba3f
Shaunak Kishore <kshaunak@gmail.com>
2015-09-28 01:29:47 -0400

    Use camelCase in server code

--- server/persistence.js
+++ server/persistence.js
@@ -3,15 +3,15 @@
 const child_process = Npm.require('child_process');
 const path = Npm.require('path');
 
-const get_backup_path = () => path.join(process.env.PWD, 'server', 'backup');
+const getBackupPath = () => path.join(process.env.PWD, 'server', 'backup');
 
 Meteor.methods({
   backup() {
-    const path = get_backup_path();
+    const path = getBackupPath();
     child_process.spawn('mongodump', ['--port', '3001', '--out', path]);
   },
   restore() {
-    const path = get_backup_path();
+    const path = getBackupPath();
     child_process.spawn('mongorestore', ['--port', '3001', '--drop', path]);
   },
 });
