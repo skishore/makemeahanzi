6592ad59dfedbc6984e9d944c11d9c8c793092d2
Shaunak Kishore <kshaunak@gmail.com>
2015-10-13 01:13:36 -0400

    Switch to Voronoi-based median computation

--- client/lib/order.js
+++ client/lib/order.js
@@ -4,75 +4,16 @@ const findPathMedian = (path) => {
   const result = [];
   const paths = svg.convertSVGPathToPaths(path);
   assert(paths.length === 1, `Got stroke with multiple loops: ${path}`);
-  const polygon = svg.getPolygonApproximation(paths[0]);
-  for (let i = 0; i < polygon.length; i++) {
-    const point1 = polygon[i];
-    const point2 = polygon[(i + 1) % polygon.length];
-    // For each side of the polygon, we compute its midpoint and then consider
-    // the portion of its perpendicular bisector that extends into the polygon.
-    // Crucially, we are using the orientation assumption here: the polygon
-    // should be oriented in the counter-clockwise direction.
-    //  - dot measures which side of the bisector we're on. If dot is greater
-    //    than dotmid, we are on the same side of the bisector as point2.
-    //  - sid measures whether a point is inside or outside the polygon. If sid
-    //    is greater than sidmid, we are inside the polygon.
-    const midpoint = Point.midpoint(point1, point2);
-    const diff = Point.subtract(point2, point1);
-    const dot = (x) => diff[0]*x[0] + diff[1]*x[1];
-    const sid = (x) => diff[0]*x[1] - diff[1]*x[0];
-    const dotmid = dot(midpoint);
-    const sidmid = sid(midpoint);
-    // For each other polygon segment, we compute the intersection of the
-    // perpendicular bisector with that segment, and track the closest one.
-    let best = undefined;
-    let best_distance = Infinity;
-    let best_tangent = undefined;
-    for (let j = 0; j < polygon.length; j++) {
-      if (j === i) continue;
-      let intersection = undefined;
-      const other1 = polygon[j];
-      const other2 = polygon[(j + 1) % polygon.length];
-      const dot1 = dot(other1) - dotmid;
-      const dot2 = dot(other2) - dotmid;
-      if (dot1 === 0 && dot2 === 0) {
-        if (Point.distance2(other1, midpoint) >
-            Point.distance2(other2, midpoint)) {
-          intersection = other2;
-        } else {
-          intersection = other1;
-        }
-      } else if (Math.sign(dot1) === Math.sign(dot2)) {
-        continue;
-      } else {
-        const t = dot1/(dot1 - dot2);
-        intersection = [(1 - t)*other1[0] + t*other2[0],
-                        (1 - t)*other1[1] + t*other2[1]];
+  const polygon = svg.getPolygonApproximation(paths[0], 32);
+  const voronoi = d3.geom.voronoi(polygon);
+  for (let cell of voronoi) {
+    const included = cell.map((x) => svg.polygonContainsPoint(polygon, x));
+    _.range(cell.length).map((i) => {
+      const j = (i + 1) % cell.length;
+      if (included[i] && included[j]) {
+        result.push([cell[i], cell[j]]);
       }
-      const distance = Math.sqrt(Point.distance2(intersection, midpoint));
-      if (distance < best_distance && sid(intersection) > sidmid) {
-        best = intersection;
-        best_distance = distance;
-        best_tangent = Point.subtract(other2, other1);
-      }
-    }
-    // If the perpendicular bisector intersects the segment opposite this one,
-    // we compute a point between the midpoint and the point of intersection
-    // that we want on the median.
-    //
-    // This point is NOT the midpoint of the midpoint and intersection. If this
-    // segment is not parallel to the opposite segment, that midpoint could be
-    // far from the median. Instead, we compute the angle bisector of the two
-    // segments and find its intersection with the perpendicular bisector.
-    if ((best_distance > 128) || (diff[0] === 0 && diff[1] === 0) ||
-        (best_tangent[0] === 0 && best_tangent[1] === 0)) {
-      continue;
-    }
-    const angle1 = Math.atan2(diff[1], diff[0]);
-    const angle2 = Math.atan2(best_tangent[1], best_tangent[0]);
-    const cos = -Math.cos(Angle.subtract(angle2, angle1));
-    const t = cos/(1 + cos);
-    result.push([(1 - t)*midpoint[0] + t*best[0],
-                 (1 - t)*midpoint[1] + t*best[1]]);
+    });
   }
   return result;
 }
@@ -87,12 +28,12 @@ stages.order = class OrderStage extends stages.AbstractStage {
     const to_path = (x) => ({d: x, fill: 'gray', stroke: 'gray'});
     Session.set('stage.paths', this.strokes.map(to_path));
     const colors = this.colors;
-    const points = [];
-    const to_point = (x, i) => {
-      const color = colors[i % colors.length];
-      return {cx: x[0], cy: x[1], fill: color, stroke: color};
+    const lines = [];
+    const to_line = (x, i) => {
+      const c = colors[i % colors.length];
+      return {x1: x[0][0], y1: x[0][1], x2: x[1][0], y2: x[1][1], stroke: c};
     }
-    this.medians.map((x, i) => x.map((y) => points.push(to_point(y, i))));
-    Session.set('stage.points', points);
+    this.medians.map((x, i) => x.map((y) => lines.push(to_line(y, i))));
+    Session.set('stage.lines', lines);
   }
 }
