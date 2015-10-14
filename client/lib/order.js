eaefd7b9bb86a6e9117b88f5a6185a5f05d9b8d5
Shaunak Kishore <kshaunak@gmail.com>
2015-10-14 00:35:24 -0400

    Get linear median computation working

--- client/lib/order.js
+++ client/lib/order.js
@@ -1,28 +1,75 @@
 "use strict";
 
-const findPathMedian = (path) => {
-  const result = [];
-  const paths = svg.convertSVGPathToPaths(path);
-  assert(paths.length === 1, `Got stroke with multiple loops: ${path}`);
-  const polygon = svg.getPolygonApproximation(paths[0], 32);
-  const voronoi = d3.geom.voronoi(polygon);
-  for (let cell of voronoi) {
-    const included = cell.map((x) => svg.polygonContainsPoint(polygon, x));
-    _.range(cell.length).map((i) => {
-      const j = (i + 1) % cell.length;
-      if (included[i] && included[j]) {
-        result.push([cell[i], cell[j]]);
+let voronoi = undefined;
+
+const findLongestShortestPath = (adjacency, vertices, node) => {
+  const path = findPathFromFurthestNode(adjacency, vertices, node);
+  return findPathFromFurthestNode(adjacency, vertices, path[0]);
+}
+
+const findPathFromFurthestNode = (adjacency, vertices, node, visited) => {
+  visited = visited || {};
+  visited[node] = true;
+  let result = [];
+  result.distance = 0;
+  for (let neighbor of adjacency[node] || []) {
+    if (!visited[neighbor]) {
+      const candidate = findPathFromFurthestNode(
+          adjacency, vertices, neighbor, visited);
+      candidate.distance +=
+          Math.sqrt(Point.distance2(vertices[node], vertices[neighbor]));
+      if (candidate.distance > result.distance) {
+        result = candidate;
       }
-    });
+    }
   }
+  result.push(node);
   return result;
+}
+
+const findStrokeMedian = (stroke) => {
+  const paths = svg.convertSVGPathToPaths(stroke);
+  assert(paths.length === 1, `Got stroke with multiple loops: ${stroke}`);
+  const polygon = svg.getPolygonApproximation(paths[0], 16);
+
+  voronoi = voronoi || new Voronoi;
+  const sites = polygon.map((point) => ({x: point[0], y: point[1]}));
+  const bounding_box = {xl: -1024, xr: 1024, yt: -1024, yb: 1024};
+  const diagram = voronoi.compute(sites, bounding_box);
+
+  diagram.vertices.map((x, i) => {
+    x.include = svg.polygonContainsPoint(polygon, [x.x, x.y]);
+    x.index = i;
+  });
+  const vertices = diagram.vertices.map((x) => [x.x, x.y].map(Math.round));
+  const edges = diagram.edges.map((x) => [x.va.index, x.vb.index]).filter(
+      (x) => diagram.vertices[x[0]].include && diagram.vertices[x[1]].include);
+  voronoi.recycle(diagram);
+
+  if (edges.length === 0) {
+    return [];
+  }
+  const adjacency = {};
+  for (let edge of edges) {
+    adjacency[edge[0]] = adjacency[edge[0]] || [];
+    adjacency[edge[0]].push(edge[1]);
+    adjacency[edge[1]] = adjacency[edge[1]] || [];
+    adjacency[edge[1]].push(edge[0]);
+  }
+  const root = edges[0][0];
+  const path = findLongestShortestPath(adjacency, vertices, root);
+  const points = path.map((i) => vertices[i]);
+
+  const tolerance = 4;
+  const simple = simplify(points.map((x) => ({x: x[0], y: x[1]})), tolerance);
+  return simple.slice(1).map((x, i) => [simple[i], x].map((x) => [x.x, x.y]));
 }
 
 stages.order = class OrderStage extends stages.AbstractStage {
   constructor(glyph) {
     super('order');
     this.strokes = glyph.stages.strokes;
-    this.medians = this.strokes.map(findPathMedian);
+    this.medians = this.strokes.map(findStrokeMedian);
   }
   refreshUI() {
     const to_path = (x) => ({d: x, fill: 'gray', stroke: 'gray'});
