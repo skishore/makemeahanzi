6252ccd8bd04c06ac304b196c84a9ae124ef6047
Shaunak Kishore <kshaunak@gmail.com>
2015-10-16 12:24:59 -0400

    Get alignment algorithm working

--- client/lib/order.js
+++ client/lib/order.js
@@ -3,6 +3,38 @@
 let stage = undefined;
 let voronoi = undefined;
 
+const size = 1024;
+
+const filterMedian = (median, n) => {
+  const distances = _.range(median.length - 1).map(
+      (i) => Math.sqrt(Point.distance2(median[i], median[i + 1])));
+  let total = 0;
+  distances.map((x) => total += x);
+  const result = [];
+  let index = 0;
+  let position = median[0];
+  let total_so_far = 0;
+  for (let i of _.range(n - 1)) {
+    const target = i*total/(n - 1);
+    while (total_so_far < target) {
+      const step = Math.sqrt(Point.distance2(position, median[index + 1]));
+      if (total_so_far + step < target) {
+        index += 1;
+        position = median[index];
+        total_so_far += step;
+      } else {
+        const t = (target - total_so_far)/step;
+        position = [(1 - t)*position[0] + t*median[index + 1][0],
+                    (1 - t)*position[1] + t*median[index + 1][1]];
+        total_so_far = target;
+      }
+    }
+    result.push(Point.clone(position));
+  }
+  result.push(median[median.length - 1]);
+  return result;
+}
+
 const findLongestShortestPath = (adjacency, vertices, node) => {
   const path = findPathFromFurthestNode(adjacency, vertices, node);
   return findPathFromFurthestNode(adjacency, vertices, path[0]);
@@ -35,7 +67,7 @@ const findStrokeMedian = (stroke) => {
 
   voronoi = voronoi || new Voronoi;
   const sites = polygon.map((point) => ({x: point[0], y: point[1]}));
-  const bounding_box = {xl: -1024, xr: 1024, yt: -1024, yb: 1024};
+  const bounding_box = {xl: -size, xr: size, yt: -size, yb: size};
   const diagram = voronoi.compute(sites, bounding_box);
 
   diagram.vertices.map((x, i) => {
@@ -75,6 +107,25 @@ const alike = (line1, line2) => {
   return Math.abs(angle) < 0.5*Math.PI;
 }
 
+const getAlignmentBounds = (alignment, tolerance) => {
+  const result = [[Math.min(alignment[0][0][0], alignment[1][0][0]),
+                   Math.min(alignment[0][0][1], alignment[1][0][1])],
+                  [Math.max(alignment[0][0][0], alignment[1][0][0]),
+                   Math.max(alignment[0][0][1], alignment[1][0][1])]];
+  tolerance = tolerance || 8;
+  _.range(2).filter((i) => result[1][i] < result[0][i] + tolerance)
+            .map((i) => result[1][i] = result[0][i] + tolerance);
+  return result;
+}
+
+const getAffineTransform = (source, target) => {
+  const sdiff = Point.subtract(source[1], source[0]);
+  const tdiff = Point.subtract(target[1], target[0]);
+  const ratio = [tdiff[0]/sdiff[0], tdiff[1]/sdiff[1]];
+  return (point) => [ratio[0]*(point[0] - source[0][0]) + target[0][0],
+                     ratio[1]*(point[1] - source[0][1]) + target[0][1]];
+}
+
 const getMedianEndpoints = (medians) => {
   const starts = medians.map((x) => [x[0], x[x.length - 1]]);
   const ends = medians.map((x) => [x[x.length - 1], x[0]]);
@@ -112,6 +163,52 @@ const getPrincipalLine = (endpoints) => {
   return best_pair;
 }
 
+const matchStrokes = (character, component) => {
+  const n = Math.max(character.length, component.length);
+  const matrix = [];
+  for (let i = 0; i < n; i++) {
+    matrix.push([]);
+    for (let j = 0; j < n; j++) {
+      if (i < component.length && j < character.length) {
+        matrix[i].push(scoreStrokes(component[i], character[j]));
+      } else {
+        matrix[i].push(i < component.length ? -size*size*size : 0);
+      }
+    }
+  }
+  let score = 0;
+  const matching = new Hungarian(matrix);
+  matching.x_match.map((j, i) => score += matrix[i][j]);
+  return {matching: matching.x_match, score: score};
+}
+
+const scoreAlignment = (character, component, alignment) => {
+  const principal = getPrincipalLine(getMedianEndpoints(component));
+  const source = getAlignmentBounds(alignment);
+  const target = getAlignmentBounds(principal);
+
+  // Compute a map from points in the character to points in the component
+  // and use it to compute a mapping between component and character strokes.
+  const transform = getAffineTransform(source, target);
+  const transformed = character.map((x) => x.map(transform));
+  const matching = matchStrokes(transformed, component);
+
+  // Compute a map from points in the component to points in the character
+  // and use it to see where the component's glyph bounds would be mapped to.
+  const inverse = getAffineTransform(target, source);
+  const border = [inverse([0, 0]), inverse([size, size])];
+
+  return matching.score;
+}
+
+const scoreStrokes = (stroke1, stroke2) => {
+  assert(stroke1.length === stroke2.length);
+  let result = 0;
+  _.range(stroke1.length).map(
+      (i) => result -= Point.distance2(stroke1[i], stroke2[i]));
+  return Math.floor(result);
+}
+
 stages.order = class OrderStage extends stages.AbstractStage {
   constructor(glyph) {
     super('order');
@@ -139,13 +236,30 @@ stages.order = class OrderStage extends stages.AbstractStage {
       return;
     }
     const glyph = Glyphs.findOne({character: components[0]});
+
+    // Compute a list of 'filtered medians' that represent the character and
+    // its component. All the filtered medians will have exactly 8 points.
     const medians = glyph.stages.strokes.map(findStrokeMedian);
-    const alignments = getPossibleAlignments(this.medians, medians);
+    const character = this.medians.map((x) => filterMedian(x, 8));
+    const component = medians.map((x) => filterMedian(x, 8));
+
+    const alignments = getPossibleAlignments(character, component);
     console.log(`Got ${alignments.length} possible alignments.`);
+    let best_alignment = undefined;
+    let best_score = -Infinity;
+    alignments.map((x) => {
+      const score = scoreAlignment(character, component, x);
+      if (score > best_score) {
+        [best_alignment, best_score] = [x, score];
+      }
+    });
+
     Meteor.setTimeout(() => {
-      const lines = Session.get('stage.lines');
-      const display = alignments.map((x) => this.alignmentToLine(x, 'red'));
-      Session.set('stage.lines', display.concat([lines[0]]));
+      //const lines = Session.get('stage.lines');
+      //const display = alignments.map((x) => this.alignmentToLine(x, 'red'));
+      //Session.set('stage.lines', display.concat([lines[0]]));
+      Session.set('stage.lines', [Session.get('stage.lines')[0],
+                                  this.alignmentToLine(best_alignment, 'red')]);
     }, 0);
   }
   refreshUI() {
@@ -157,7 +271,8 @@ stages.order = class OrderStage extends stages.AbstractStage {
       const color = colors[i % colors.length];
       return {cx: x[0], cy: x[1], fill: color, stroke: color};
     }
-    this.medians.map((x, i) => x.map((y) => points.push(to_point(y, i))));
+    this.medians.map((x) => filterMedian(x, 8))
+                .map((x, i) => x.map((y) => points.push(to_point(y, i))));
     Session.set('stage.points', points);
     Session.set('stage.lines', [this.alignmentToLine(this.principal, 'black')]);
   }
