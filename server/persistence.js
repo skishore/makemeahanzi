e4a5db27602a049b8c4e41f9206e4416f4b5b91a
Shaunak Kishore <kshaunak@gmail.com>
2015-10-21 00:28:50 -0400

    Handle lack of process.env.PWD in Windows node

--- server/persistence.js
+++ server/persistence.js
@@ -3,7 +3,15 @@
 const child_process = Npm.require('child_process');
 const path = Npm.require('path');
 
-const getBackupPath = () => path.join(process.env.PWD, 'server', 'backup');
+const getBackupPath = () => {
+  // TODO(skishore): The next line makes assumptions about the Meteor build
+  // directory's structure. We should replace it with a Meteor-provided API.
+  let pwd = path.join(process.cwd(), '../../../..');
+  if (process.env && process.env.PWD) {
+    pwd = process.env.PWD;
+  }
+  return path.join(pwd, 'server', 'backup');
+}
 
 Meteor.methods({
   backup() {
