679f0af52edd7e10507b37c79ff726d3de418bc7
Shaunak Kishore <kshaunak@gmail.com>
2015-10-27 23:30:05 -0400

    Get re-ordering UI working

--- client/lib/order.js
+++ client/lib/order.js
@@ -2,6 +2,8 @@
 
 let stage = undefined;
 
+const Order = new Mongo.Collection('order')._collection;
+
 // TODO(skishore): Consider using sqrt(1/2) in place of 1/2 here. This constant
 // is used to compute bounds for components that are surrounded.
 const rad2 = 1/2;
@@ -132,45 +134,34 @@ const scoreStrokes = (stroke1, stroke2) => {
   return Math.max(option1, option2);
 }
 
-// This method adds two extra fields to each element of the stroke-order list:
-//   - component: The character of the component that stroke belongs to.
-//                Set to '?' for strokes that were not matched.
-//   - index: The index of that component in the list of components.
-//            Set to -1 for strokes that were not matched.
-
-const augmentOrderListWithComponentData = (order, tree) => {
-  const indices = {null: -1};
-  return (order || []).map((x) => {
-    const component = x.match ?
-        decomposition_util.getSubtree(tree, x.match).value : '?';
-    const key = JSON.stringify(x.match || null);
-    const index = indices.hasOwnProperty(key) ?
-        indices[key] : Object.keys(indices).length - 1;
-    indices[key] = index;
-    return _.extend({component: component, index: index}, x);
-  });
-}
-
 stages.order = class OrderStage extends stages.AbstractStage {
   constructor(glyph) {
     super('order');
     this.matching = undefined;
     this.medians = glyph.stages.strokes.map(median_util.findStrokeMedian);
+    this.strokes = glyph.stages.strokes;
+
     const tree = decomposition_util.convertDecompositionToTree(
         glyph.stages.analysis.decomposition);
     this.tree = augmentTreeWithBoundsData(tree, [[0, 0], [1, 1]]);
+
+    this.indices = {null: -1};
+    this.components = [];
+    this.paths = [];
+    collectComponentNodes(this.tree).map((x, i) => {
+      this.indices[JSON.stringify(x.path)] = i;
+      this.components.push(x.value);
+      this.paths.push(x.path);
+    });
+
     stage = this;
   }
   handleEvent(event, template) {
-    let matches = {};
-    let max = -1;
-    const order = augmentOrderListWithComponentData(this.order, this.tree);
-    order.map((x) => {
-      matches[x.index] = x.match;
-      max = Math.max(max, x.index);
-    });
-    const index = ((order[template.i].index + 2) % (max + 2)) - 1;
-    this.order[template.i].match = matches[index];
+    const element = this.order.filter(
+        (x) => x.stroke === template.stroke_index)[0];
+    const old_index = this.indices[JSON.stringify(element.match || null)];
+    const new_index = ((old_index + 2) % (this.components.length + 1)) - 1;
+    element.match = this.paths[new_index];
     this.forceRefresh();
   }
   onAllComponentsReady() {
@@ -194,6 +185,12 @@ stages.order = class OrderStage extends stages.AbstractStage {
     });
     this.forceRefresh();
   }
+  onSort(old_index, new_index) {
+    const elements = this.order.splice(old_index, 1);
+    assert(elements.length === 1);
+    this.order.splice(new_index, 0, elements[0]);
+    this.forceRefresh();
+  }
   refreshUI() {
     Session.set('stage.status', [{
       cls: this.order ? 'success' : 'error',
@@ -201,49 +198,77 @@ stages.order = class OrderStage extends stages.AbstractStage {
           'Stroke order determined by decomposition.' :
           'Loading component data...',
     }]);
-    Session.set('stages.order.components',
-                decomposition_util.collectComponents(this.tree));
-    Session.set('stages.order.matching', {
-      colors: this.colors,
-      order: augmentOrderListWithComponentData(this.order, this.tree),
+    Session.set('stages.order.colors', this.colors);
+    Session.set('stages.order.components', this.components);
+    Session.set('stages.order.indices', this.indices);
+    Session.set('stages.order.order', this.order);
+    Order.remove({});
+    (this.order || []).map((x, i) => {
+      const key = JSON.stringify(x.match || null);
+      Order.insert({
+        color: this.colors[this.indices[key]] || 'lightgray',
+        glyph: [{d: this.strokes[x.stroke]}],
+        stroke_index: x.stroke,
+        index: i,
+      });
     });
   }
 }
 
 Template.order_stage.helpers({
   character: () => {
-    const matching = Session.get('stages.order.matching') || {};
+    const colors = Session.get('stages.order.colors');
+    const indices = Session.get('stages.order.indices');
+    const order = Session.get('stages.order.order');
     const character = Session.get('editor.glyph');
     const result = [];
-    (matching.order || []).map((order, i) => {
-      const color = matching.colors[order.index % matching.colors.length];
+    if (!colors || !indices || !order || !character) {
+      return result;
+    }
+    for (let element of order) {
+      const index = indices[JSON.stringify(element.match || null)];
+      const color = colors[index % colors.length];
       result.push({
         cls: 'selectable',
-        d: character.stages.strokes[order.stroke],
-        fill: order.index < 0 ? 'lightgray' : color,
-        stroke: order.index < 0 ? 'lightgray' : 'black',
-        i: i,
+        d: character.stages.strokes[element.stroke],
+        fill: index < 0 ? 'lightgray' : color,
+        stroke: index < 0 ? 'lightgray' : 'black',
+        stroke_index: element.stroke,
       });
-    });
+    }
     return result;
   },
   components: () => {
-    const matching = Session.get('stages.order.matching') || {};
+    const colors = Session.get('stages.order.colors');
+    const components = Session.get('stages.order.components');
     const result = [];
-    for (let order of matching.order || []) {
-      if (order.index < result.length) {
-        continue;
-      }
-      const color = matching.colors[order.index % matching.colors.length];
-      const glyph = Glyphs.findOne({character: order.component});
+    if (!colors || !components) {
+      return result;
+    }
+    for (let index = 0; index < components.length; index++) {
+      const color = colors[index % colors.length];
+      const glyph = Glyphs.findOne({character: components[index]});
       const component = [];
       for (let stroke of glyph.stages.strokes) {
         component.push({d: stroke, fill: color, stroke: 'black'});
       }
-      component.top = `${138*order.index + 8}px`;
+      component.top = `${138*index + 8}px`;
       result.push(component);
     }
     return result;
+  },
+  items: () => {
+    return Order.find({}, {limit: stage ? stage.medians.length : 0});
+  },
+  options: () => {
+    return {
+      onSort: (event) => {
+        // Suppress the two errors that will be printed when the Sortable
+        // plugin tries to persist the sort result to the server.
+        Meteor._suppress_log(2);
+        stage && stage.onSort(event.oldIndex, event.newIndex);
+      },
+    }
   },
 });
 
