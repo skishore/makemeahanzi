7b493f8b18217b1a9f1658890f75c75c9d21f9c9
Shaunak Kishore <kshaunak@gmail.com>
2015-11-09 00:06:08 -0500

    Add method to migrate glyphs from old schema

--- server/migration.js
+++ server/migration.js
@@ -8,11 +8,29 @@ const checkStrokeExtractorStability = (glyph) => {
   }
 }
 
+const convertOldPathSchemaToSVGPath = (path) => {
+  const terms = [];
+  for (let segment of path) {
+    assert('LMQZ'.indexOf(segment.type) >= 0, segment.type);
+    terms.push(segment.type);
+    if (segment.x1 !== undefined) {
+      terms.push(segment.x1);
+      terms.push(segment.y1);
+    }
+    if (segment.x !== undefined) {
+      terms.push(segment.x);
+      terms.push(segment.y);
+    }
+  }
+  return terms.join(' ');
+}
+
 const migrateOldGlyphSchemaToNew = (glyph) => {
   const codepoint = parseInt(glyph.name.substr(3), 16);
   const character = String.fromCodePoint(codepoint);
   const data = cjklib.getCharacterData(character);
-  assert(glyph.manual.verified !== undefined);
+  assert(glyph.manual && glyph.manual.verified !== undefined,
+         `Glyph ${character} was not verified.`);
   const result = {
     character: character,
     codepoint: codepoint,
@@ -23,7 +41,7 @@ const migrateOldGlyphSchemaToNew = (glyph) => {
       strokes: undefined,
     },
     stages: {
-      path: Glyphs.get_svg_path(glyph),
+      path: convertOldPathSchemaToSVGPath(glyph.path),
       bridges: glyph.manual.bridges,
       strokes: glyph.derived.strokes,
       analysis: undefined,
@@ -42,13 +60,38 @@ const setAnalysisStageToReady = (glyph) => {
   if (data && data.strokes === glyph.stages.strokes.length) {
     glyph.stages.analysis = {sentinel: true};
   } else {
-    console.log(`Different stroke count for ${glyph.character}`);
+    console.log(`Different stroke count for ${glyph.character}. Expected: ` +
+                `${data.strokes}, got: ${glyph.stages.strokes.length}.`);
   }
 }
 
 const completionCallback = undefined;
 
 const perGlyphCallback = undefined;
+
+const loadFromOldSchemaJSON = (filename) => {
+  const fs = Npm.require('fs');
+  const path = Npm.require('path');
+  const filepath = path.join(getPWD(), 'public', filename);
+  fs.readFile(filepath, 'utf8', Meteor.bindEnvironment((error, data) => {
+    if (error) throw error;
+    const lines = data.split('\n').filter((x) => x.length > 0);
+    console.log(`Loaded ${lines.length} old-schema glyphs.`);
+    let migrated = 0;
+    for (var line of lines) {
+      try {
+        const old_glyph = JSON.parse(line);
+        const new_glyph = migrateOldGlyphSchemaToNew(old_glyph);
+        setAnalysisStageToReady(new_glyph);
+        Glyphs.save(new_glyph);
+        migrated += 1;
+      } catch (error) {
+        console.error(error);
+      }
+    }
+    console.log(`Successfully migrated ${migrated} glyphs.`);
+  }));
+}
 
 // Runs the given per-glyph callback for each glyph in the database.
 // When all the glyphs are migrated, runs the completion callback.
@@ -71,6 +114,14 @@ const runMigration = () => {
   }
   console.log('Migration complete.');
 }
+
+Meteor.methods({
+  'loadFromOldSchemaJSON': (filename) => {
+    cjklib.promise.then(
+        Meteor.bindEnvironment(() => loadFromOldSchemaJSON(filename)))
+                  .catch(console.error.bind(console));
+  },
+});
 
 Meteor.startup(() => {
   if (!perGlyphCallback && !completionCallback) {
--- server/persistence.js
+++ server/persistence.js
@@ -4,13 +4,7 @@ const child_process = Npm.require('child_process');
 const path = Npm.require('path');
 
 const getBackupPath = () => {
-  // TODO(skishore): The next line makes assumptions about the Meteor build
-  // directory's structure. We should replace it with a Meteor-provided API.
-  let pwd = path.join(process.cwd(), '../../../..');
-  if (process.env && process.env.PWD) {
-    pwd = process.env.PWD;
-  }
-  return path.join(pwd, 'server', 'backup');
+  return path.join(getPWD(), 'server', 'backup');
 }
 
 Meteor.methods({
