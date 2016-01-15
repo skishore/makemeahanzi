81da7c195fe3d485ffc2fa947a19eccdfe2c9c78
Shaunak Kishore <kshaunak@gmail.com>
2016-01-15 18:27:40 -0500

    Rework script to produce binary output

--- main.py
+++ main.py
@@ -1,14 +1,30 @@
+#!/usr/bin/python
 import json
 
-normalization = 16
+# Serializes a single median into a binary format.
+def encode_median(median):
+  result = []
+  result.append(chr(len(median)))
+  for (x, y) in median:
+    result.append(chr(x/4))
+    result.append(chr((900 - y)/4))
+  return ''.join(result)
 
-def normalize(median):
-  return map(lambda x: (x[0]/normalization, x[1]/normalization), median)
+# Serializes a character and its medians into a binary format.
+def encode(row):
+  result = []
+  encoded_character = row['character'].encode('utf16')
+  result.append(chr(len(encoded_character)))
+  result.append(encoded_character)
+  result.append(chr(len(row['medians'])))
+  result.extend(map(encode_median, row['medians']))
+  return ''.join(result)
 
-characters = []
-for line in open('makemeahanzi.txt').readlines():
-  character = json.loads(line.strip())
-  characters.append([character['character'],
-                     map(normalize, character['medians'])])
-open('medians.js', 'w').write(
-    'medians = %s;' % (json.dumps(characters).replace(' ', ''),))
+if __name__ == '__main__':
+  with open('makemeahanzi.txt') as input, \
+       open('medians.bin', 'w') as output:
+    result = input.readline()
+    while result:
+      row = json.loads(result.strip())
+      output.write(encode(row))
+      result = input.readline()
