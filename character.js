9ee4a087594c528bbb9c9860825147f50561067d
Shaunak Kishore <kshaunak@gmail.com>
2016-01-30 14:02:11 -0500

    Add code to render decomposition trees

--- character.js
+++ character.js
@@ -1,5 +1,15 @@
 "use strict";
 
+const augmentTreeWithLabels = (node) => {
+  const value = node.value;
+  if (node.type === 'compound') {
+    node.label = `${value} - ${decomposition_util.ids_data[value].label}`;
+    node.children.map((child) => augmentTreeWithLabels(child));
+  } else {
+    node.label = `${value} - another character`;
+  }
+}
+
 const coerceToUnicode = (character) => {
   if (character.startsWith('&#') && character.endsWith(';')) {
     const char_code = parseInt(character.slice(2, character.length - 1), 10);
@@ -8,8 +18,27 @@ const coerceToUnicode = (character) => {
   return character;
 }
 
+const constructTree = (decomposition) => {
+  const tree = decomposition_util.convertDecompositionToTree(decomposition);
+  augmentTreeWithLabels(tree);
+  return tree;
+}
+
+const getCharacterData = ($http, character, callback) => {
+  const part = Math.floor(character.charCodeAt(0) / 256);
+  $http.get(`data/part-${part}.txt`).then((response) => {
+    const data = response.data;
+    for (let row of response.data) {
+      if (row.character === character) {
+        return callback(row);
+      }
+    }
+  });
+}
+
 const DataController = function($scope, $routeParams, $http) {
   this.character = coerceToUnicode($routeParams.character);
+  this.decomposition = [];
   this.metadata = [];
   this.strokes = [];
 
@@ -19,6 +48,7 @@ const DataController = function($scope, $routeParams, $http) {
   this.class = this._class();
 
   this._refresh = (row) => {
+    this.decomposition.push(constructTree(row.decomposition));
     this.metadata = [
       {label: 'Definition:', value: row.definition},
       {label: 'Pinyin:', value: row.pinyin.join(', ')},
@@ -26,17 +56,7 @@ const DataController = function($scope, $routeParams, $http) {
     ];
     this.strokes = row.strokes;
   }
-
-  const part = Math.floor(this.character.charCodeAt(0) / 256);
-  $http.get(`data/part-${part}.txt`).then((response) => {
-    const data = response.data;
-    for (let row of response.data) {
-      if (row.character === this.character) {
-        this._refresh(row);
-        break;
-      }
-    }
-  });
+  getCharacterData($http, this.character, this._refresh.bind(this));
 }
 
 angular.module('makemeahanzi')
