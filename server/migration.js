9607f920a94704ea53c1a31760cb1a942a1d31ff
Shaunak Kishore <kshaunak@gmail.com>
2016-02-09 00:49:28 -0500

    Split data between two license sources

--- server/migration.js
+++ server/migration.js
@@ -38,7 +38,7 @@ const convertOldPathSchemaToSVGPath = (path) => {
   return terms.join(' ');
 }
 
-const dumpGlyph = (stream) => (glyph) => {
+const dumpGlyph = (dictionary, graphics) => (glyph) => {
   if (!glyph.stages.verified) {
     return;
   }
@@ -53,90 +53,22 @@ const dumpGlyph = (stream) => (glyph) => {
   medians.map((x) => assert(x));
   const has_etymology =
       analysis.etymology.hint || (analysis.etymology.type === 'pictophonetic');
-  const result = {
-    // Unicode character for this glyph. Required.
+
+  dictionary.write(JSON.stringify({
     character: glyph.character,
-    // String definition targeted towards second-language learners. Optional.
     definition: glyph.metadata.definition || data.definition,
-    // Comma-separated list of pronunciations of this character. May be empty.
     pinyin: pinyin,
-    // Ideograph Description Sequence decomposition of the character. See:
-    // https://en.wikipedia.org/wiki/Chinese_character_description_languages#Ideographic_Description_Sequences
-    //
-    // Optional. Invalid if it starts with a full-width question mark '？'.
-    // Note that even if the first character is a proper IDS symbol, any
-    // component within the decomposition may be a wide question mark as well.
-    // For example, if we have a decomposition of a character into a top and
-    // bottom component but can only recognize the top component, we might
-    // have a decomposition like so: '⿱逢？'
-    decomposition: analysis.decomposition,
-    // An etymology for the character. This field may be null. If present,
-    // it will always have a "type" field, which will be one of "ideographic",
-    // "pictographic", or "pictophonetic".
-    //
-    // If the type is one of the first two options, then the etymology will
-    // always include a string "hint" field explaining its formation.
-    //
-    // If the type is "pictophonetic", then the etymology will contain three
-    // other fields: "hint", "phonetic", and "semantic", each of which is
-    // a string and each of which may be null. The etymology should be read as:
-    //   ${semantic} (${hint}) provides the meaning while ${phonetic}
-    //   provides the pronunciation.
-    // with allowances for possible null values.
+    decomposition: analysis.decomposition || '？',
     etymology: has_etymology ? analysis.etymology : undefined,
-    // Unicode primary radical for this character. Required.
     radical: analysis.radical,
-    // List of SVG path data for each stroke of this character, ordered by
-    // proepr stroke order. Each stroke is laid out on a 1024x1024 size
-    // coordinate system where:
-    //   - The upper-left corner is at position (0, 900)
-    //   - The lower-right corner is at position (1024, 900)
-    // Note that the y-axes DECREASES as you move downwards, which is strage!
-    // To display these paths properly, you should hide render them as follows:
-    //   <svg viewBox="0 0 1024 1024">
-    //     <g transform="scale(1, -1) translate(0, -900)">
-    //       <path d="STROKE[0] DATA GOES HERE"></path>
-    //       <path d="STROKE[1] DATA GOES HERE"></path>
-    //       ...
-    //     </g>
-    //   </svg>
-    strokes: strokes,
-    // A list of stroke medians, in the same coordinate system as the SVG
-    // paths above. These medians can be used to produce a rough stroke-order
-    // animation, although it is a bit tricky.
-    //
-    // Each median is a list of pairs of integers.
-    // This list will be as long as the strokes list.
-    medians: medians,
-    // A list of stroke medians, normalized to be in a sane coordinate system
-    // so that they can be used for handwriting recognition:
-    //   - The upper-left corner is at position (0, 0)
-    //   - The lower-right corner is at position (1, 1)
-    //
-    // Each normalized median is a list of pairs of floating-point numbers.
-    // This list will be as long as the strokes list.
-    normalized_medians: medians.map(median_util.normalizeForMatch),
-    // A list of mappings from strokes of this character to strokes of its
-    // components, as indexed in its decomposition tree. Any given entry in
-    // this list may be null. If an entry is not null, it will be a list of
-    // indices corresponding to a path down the decomposition tree.
-    //
-    // This schema is a little tricky to explain without an example. Suppose
-    // that the character '俢' has the decomposition: '⿰亻⿱夂彡'
-    //
-    // The third stroke in that character belongs to the radical '夂'.
-    // Its match would be [1, 0]. That is, if you think of the decomposition as
-    // a tree, it has '⿰' at its root with two children '亻' and '⿱', and
-    // '⿱' further has two children '夂' and '彡'. The path down the tree
-    // to '夂' is to take the second child of '⿰' and the first of '⿱',
-    // hence, [1, 0].
-    //
-    // This field can be used to generate visualizations marking each component
-    // within a given character, or potentially for more exotic purposes.
     matches: order.map((x) => x.match),
-  }
-  stream.write(JSON.stringify(result));
-  stream.write('\n');
+  }) + '\n');
+  graphics.write(JSON.stringify({
+    character: glyph.character,
+    strokes: strokes,
+    medians: medians,
+    normalized_medians: medians.map(median_util.normalizeForMatch),
+  }) + '\n');
 }
 
 const migrateOldGlyphSchemaToNew = (glyph) => {
@@ -187,9 +119,15 @@ const migrateOldGlyphSchemaToNew = (glyph) => {
 const dumpToNewSchemaJSON = () => {
   const fs = Npm.require('fs');
   const path = Npm.require('path');
-  const filepath = path.join(getPWD(), 'server', 'makemeahanzi.txt');
-  const stream = fs.createWriteStream(filepath);
-  runMigration(dumpGlyph(stream), (() => stream.end()));
+  const directory = path.join(getPWD(), 'server', 'release');
+  const dictionary = fs.createWriteStream(
+      path.join(directory, 'dictionary.txt'));
+  const graphics = fs.createWriteStream(
+      path.join(directory, 'graphics.txt'));
+  runMigration(dumpGlyph(dictionary, graphics), (() => {
+    dictionary.end();
+    graphics.end();
+  }));
 }
 
 const loadFromOldSchemaJSON = (filename) => {
