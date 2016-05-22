1a9986aaeb2c2d0c8dbae05d910051230afdc45d
Shaunak Kishore <kshaunak@gmail.com>
2016-05-22 17:08:15 -0400

    Improve Settings.setDefault safety

--- model/settings.js
+++ model/settings.js
@@ -3,26 +3,21 @@
 //  - value: any
 const settings = new Ground.Collection('settings', {connection: null});
 
+const defaults = {
+  'settings.double_tap_speed': 500,
+  'settings.paper_filter': true,
+  'settings.reveal_order': true,
+  'settings.snap_strokes': true,
+};
+
 class Settings {
   static get(key) {
     const record = settings.findOne({key: key});
-    return record ? record.value : undefined;
+    return record ? record.value : defaults[key];
   }
   static set(key, value) {
     settings.upsert({key: key}, {$set: {key: key, value: value}});
   }
-  static setDefault(key, value) {
-    settings.upsert({key: key}, {$setOnInsert: {key: key, value: value}});
-  }
 }
-
-Meteor.startup(() => Tracker.autorun(() => {
-  if (Meteor.isClient && Ground.ready()) {
-    Settings.setDefault('settings.double_tap_speed', 500);
-    Settings.setDefault('settings.paper_filter', true);
-    Settings.setDefault('settings.reveal_order', true);
-    Settings.setDefault('settings.snap_strokes', true);
-  }
-}));
 
 export {Settings};
