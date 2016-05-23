e0193530c8ed7db83789c1e67f5c0ddc21435e36
Shaunak Kishore <kshaunak@gmail.com>
2016-05-22 22:26:25 -0400

    Fix critical bugs in Ground readiness check

--- model/model.js
+++ model/model.js
@@ -2,26 +2,37 @@
 // The most important methods here are autorun and startup, which have the
 // same contract as Tracker.autorun and Meteor.startup but wait for the
 // database to be ready before executing.
+const ready = new ReactiveVar();
 const collections = {};
+const loaded = {};
 
 class Model {
   static autorun(callback) {
     Meteor.startup(() => Tracker.autorun(() =>
-        Meteor.isClient && Ground.ready() && callback()));
+        Meteor.isClient && ready.get() && callback()));
   }
   static collection(name) {
     collections[name] = new Ground.Collection(name, {connection: null});
     return collections[name];
   }
   static startup(callback) {
-    this.autorun(() => Meteor.setTimeout(() => callback()));
+    let done = false;
+    this.autorun(() => Meteor.setTimeout(
+        () => done = done || callback() || true));
   }
   static timestamp() {
     return Math.floor(new Date().getTime() / 1000);
   }
 }
 
-// Expose the collections for easy debugging.
-if (Meteor.isClient) window.collections = collections;
+// On the client, expose all registered collections for easy debugging and
+// set the ready bit when all collections are loaded from localStorage.
+if (Meteor.isClient) {
+  Ground.addListener(['loaded'], (event) => {
+    loaded[event.collection] = true;
+    ready.set(_.all(_.keys(collections).map((x) => loaded[x])));
+  });
+  window.collections = collections;
+}
 
 export {Model}
