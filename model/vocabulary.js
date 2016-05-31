b3a7ce764a413b232979807cc9a0e50cd9849355
Shaunak Kishore <kshaunak@gmail.com>
2016-05-31 11:31:27 -0400

    HAX: Switch to in-memory vocabulary store

--- model/vocabulary.js
+++ model/vocabulary.js
@@ -8,64 +8,119 @@
 //  - successes: number of times the user has gotten the word right
 //  - failed: true if this item should be shown again in the failures deck
 //
-// The "correct" and "update" model methods take a "result" argument which
-// can be anything in the set {0, 1, 2, 3}, with higher numbers indicating
-// that the user made more errors.
+// The "updateItem" model method takes a "result" argument which should be a
+// value in the set {0, 1, 2, 3}, with higher numbers indicating that the
+// user made more errors.
 import {getNextInterval} from './external/interval_quantifier';
 import {Model} from './model';
 
-const vocabulary = Model.collection('vocabulary');
+const kColumns = 'word last next lists attempts successes failed'.split(' ');
+const kIndices = {};
+kColumns.map((x, i) => kIndices[x] = i);
+
+const entries = {active: [], all: []};
+const index = {};
+const sentinel = new ReactiveVar();
+
+const dirty = () => sentinel.set(sentinel.get() + 1);
+
+const materialize = (entry) => {
+  const result = {};
+  kColumns.map((x, i) => result[x] = entry[i]);
+  return result;
+}
+
+class Cursor {
+  constructor(filter) {
+    sentinel.get();
+    this._list = entries.active.filter(filter);
+  }
+  count() {
+    return this._list.length;
+  }
+  fetch() {
+    return this._list.map(materialize);
+  }
+  forEach(callback) {
+    this.fetch().forEach(callback);
+  }
+}
 
 class Vocabulary {
   static addItem(word, list) {
-    // TODO(skishore): This method runs in time O(n) because of the upsert.
-    // Implement a batch addList method that does not suffer this penalty.
-    const update = {
-      $addToSet: {lists: list},
-      $setOnInsert: {word: word, attempts: 0, successes: 0},
-    };
-    vocabulary.upsert({word: word}, update);
+    if (!index[word]) {
+      const entry = [word, null, null, [], 0, 0, false];
+      if (entry.length !== kColumns.length) throw new Error(entry);
+      index[word] = entry;
+      entries.all.push(entry);
+    }
+    const entry = index[word];
+    const lists = entry[kIndices.lists];
+    if (lists.indexOf(list) < 0) {
+      lists.push(list);
+      if (lists.length === 1) entries.active.push(entry);
+    }
+    dirty();
   }
   static clearFailed(item) {
-    vocabulary.update({word: item.word}, {$set: {failed: false}});
+    const entry = index[item.word];
+    if (entry) entry[kIndices.failed] = false;
+    dirty();
   }
   static dropList(list) {
-    vocabulary.update({}, {$pull: {lists: list}}, {multi: true});
-    vocabulary.remove({lists: [], last: {$exists: false}});
+    const updated = {active: [], all: []};
+    entries.all.forEach((entry) => {
+      const lists = entry[kIndices.lists].filter((x) => x !== list);
+      if (lists.length + entry[kIndices.attempts] > 0) {
+        entry[kIndices.lists] = lists;
+        updated.all.push(entry);
+        if (lists.length > 0) updated.active.push(entry);
+      } else {
+        delete index[entry[kIndices.word]];
+      }
+    });
+    entries.active = updated.active;
+    entries.all = updated.all;
+    dirty();
   }
   static getFailuresInRange(start, end) {
-    return vocabulary.find({
-      last: {$exists: true, $gte: start, $lt: end},
-      lists: {$ne: []},
-      failed: true,
+    return new Cursor((entry) => {
+      if (!entry[kIndices.failed]) return false;
+      const last = entry[kIndices.last];
+      return start <= last && last < end;
     });
   }
   static getItemsDueBy(last, next) {
-    return vocabulary.find({
-      last: {$exists: true, $lt: last},
-      next: {$exists: true, $lt: next},
-      lists: {$ne: []},
+    return new Cursor((entry) => {
+      if (entry[kIndices.attempts] === 0) return false;
+      return entry[kIndices.last] < last && entry[kIndices.next] < next;
     });
   }
   static getNewItems() {
-    return vocabulary.find({lists: {$ne: []}, last: {$exists: false}});
+    return new Cursor((entry) => entry[kIndices.attempts] === 0);
   }
   static updateItem(item, result, correction) {
+    const entry = index[item.word];
+    const expected = item.attempts + (correction ? 1 : 0);
+    if (!entry || entry[kIndices.attempts] !== expected) return;
+
     const last = Model.timestamp();
-    const next = last + getNextInterval(item, result, last);
+    entry[kIndices.last] = last;
+    entry[kIndices.next] = last + getNextInterval(item, result, last);
+
     const success = result < 3;
-    const update = {
-      $set: {
-        last: last,
-        next: next,
-        attempts: item.attempts + 1,
-        successes: item.successes + (success ? 1 : 0),
-        failed: !success,
-      },
-    };
-    attempts = item.attempts + (correction ? 1 : 0);
-    vocabulary.update({word: item.word, attempts: attempts}, update);
+    entry[kIndices.attempts] = item.attempts + 1;
+    entry[kIndices.successes] = item.successes + (success ? 1 : 0);
+    entry[kIndices.failed] = !success;
+    dirty();
   }
+}
+
+if (Meteor.isClient) {
+  window.entries = entries;
+  window.index = index;
+  window.sentinel = sentinel;
+  window.Vocabulary = Vocabulary;
 }
 
 export {Vocabulary};
